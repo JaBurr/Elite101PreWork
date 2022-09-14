9bd58821b30a884db2e87ccd1017166a2b226ad5
Ja'Qwan Burrill <113491283+JaBurr@users.noreply.github.com>
2022-09-14 20:03:38 +0000

    Intro Edit.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-input("Hello, I am Gato. I am a bot that takes your responses and adds them to my list of responses. The more you type, the more I learn. How is your day?\n")
+input("Hello, I am Gato. I am a bot that takes your responses and adds them to my list of responses. The more you type, the more I learn.\n")
 #line added on replit
 
 responses = [
